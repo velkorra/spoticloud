26f4dc1ca82628f8090b1c2a34661c2098e486bd
vertunoff <anton.s.vertunov@gmail.com>
2024-07-06 02:21:03 +0300

    spring initialized!

---
+++ src/main/java/com/example/spoticloud/SpoticloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.spoticloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoticloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpoticloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spoticloud/SpoticloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spoticloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoticloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
